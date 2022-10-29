e033eea34bb255ace27446f518f7f9283d17112c
K-RKY <r8613k@icloud.com>
2022-10-29 15:08:09 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
+  has_many :items
 end
