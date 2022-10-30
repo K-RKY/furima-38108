805f108d8633a06af40bfa138a9e1cb6bfe29255
K-RKY <r8613k@icloud.com>
2022-10-30 18:37:57 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :shipping_area
+  belongs_to :shipping_days
+  has_one_attached :image
 
-  validates :name, :text, :postage_id, :category_id
-            :shipping_area_id, :shipping_days_id, :condition_id
-            :user_id, presence: true
-  validates :price, presence: true, format: {with:/\A[-]?[0-9]+(\.[0-9]+)?\z/},
-            :greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
+  validates :name, :image, :text, :user_id, presence: true
+  validates :price, presence: true, format: {with:/\A[-]?[0-9]+(\.[0-9]+)?\z/}, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :postage_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :shipping_days_id, numericality: { other_than: 1 }
 end
