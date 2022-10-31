7dafa805af2a16129871045a2367dab8363c3d07
K-RKY <r8613k@icloud.com>
2022-10-31 09:36:15 +0900

    テストコードの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_days
   has_one_attached :image
 
-  validates :name, :image, :text, :user_id, presence: true
+  validates :name, :image, :text, presence: true
   validates :price, presence: true, format: {with:/\A[-]?[0-9]+(\.[0-9]+)?\z/}, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品を出品出来ない時' do
+      it 'nameが無ければ出品出来ない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Nameを入力してください")
+      end
+      it 'imageが無ければ出品出来ない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Imageを入力してください")
+      end
+      it 'textが無ければ出品出来ない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Textを入力してください")
+      end
+      it 'userと紐づいていなければ出品出来ない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'priceが無ければ出品出来ない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Priceを入力してください")
+      end
+      it 'priceは全て半角数値出なければ登録できない' do
+        @item.price = 'abcd'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Priceは数値で入力してください")
+      end
+      it 'priceは299以下では登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Priceは300以上の値にしてください")
+      end
+      it 'priceは10000000以上では登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Priceは9999999以下の値にしてください")
+      end
+      it 'category_idが1だと登録出来ない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Categoryは1以外の値にしてください")
+      end
+      it 'condition_idが1だと登録出来ない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Conditionは1以外の値にしてください")
+      end
+      it 'postage_idが1だと登録出来ない' do
+        @item.postage_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postageは1以外の値にしてください")
+      end
+      it 'shipping_area_idが0だと登録出来ない' do
+        @item.shipping_area_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping areaは0以外の値にしてください")
+      end
+      it 'shipping_days_idが1だと登録出来ない' do
+        @item.shipping_days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping daysは1以外の値にしてください")
+      end
+    end
+    context '商品を出品出来る時' do
+      it '全ての情報を登録すると出品出来る' do
+        expect(@item).to be_valid
+      end
+
+    end
+    
+
+  end
+
+  
 end
