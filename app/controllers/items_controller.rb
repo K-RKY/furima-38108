02811e83b5883b9f67b7c60b70e4ee034f120fbd
K-RKY <r8613k@icloud.com>
2022-11-01 19:39:58 +0900

    コードレビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-   if @item.destroy
-    redirect_to root_path
+   if @item.user_id == current_user.id
+       @item.destroy
+        redirect_to root_path
    end
   end
 
